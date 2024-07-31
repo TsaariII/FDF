5efabfb974eedb787bb897bab28e01ad63d509b2
¨Nikolai <nzharkev@student.hive.fi>
2024-07-31 15:17:07 +0300

    Makefile and created .h file

--- Makefile
+++ Makefile
@@ -1,7 +1,52 @@
-NAME = fdf
+NAME := fdf
 
 FLAGS = -Wall -Werror -Wextra
 
-MLX = ./lib/MLX42
+OFLAGS = -ldl -lglfw -pthread -lm
+
+LIBFT = $(LIBFT_DIR)/libft.a
+
+LIBFT_DIR = ./libft
+
+MLX = ./MLX42
+
+MLX_HEAD = -I $(MLX_DIR)/include
+
+LIBMLX = $(MLX)/build/libmlx42.a
+
+HEADS = -I. $(MLX_HEAD) -I$(LIBFT_DIR)
+
+$(LIBMLX):
+		@if [! -d "$(MLX_DIR)"]; then \
+			git clone https://github.com/codam-coding-college/MLX42.git $(MLX_DIR); \
+		fi
+		@if [! -d "$(MLX_DIR)/build"]; then \
+			cmake $(MLX_DIR) -B $(MLX_DIR)/build; \
+		fi
+		@make -C $(MLX_DIR)/build -j4
 
 
+
+OBJS = $(SRC: .c=.o)
+
+all: $(LIBFT) $(LIBMLX) $(NAME)
+
+%.o: %.c
+		cc $(FLAGS) -c $< -o $@ $(HEADS)
+
+$(NAME): $(OBJS)
+		@cc $(OBJS) $(LIBMLX) $(LIBFT) $(OFLAGS) -o $(NAME)
+
+clean:
+		@rm -rf $(OBJS)
+		@rm -rf $(MLX)/build
+		@$(MAKE) -C $(LIBFT_DIR) clean
+
+fclean:
+		rm -rf $(NAME)
+		@$(MAKE) -C $(LIBFT_DIR) fclean
+		rm -rf $(MLX)
+
+re: fclean all
+
+.PHONY: all clean fclean re libmlx libft
--- fdf.h
+++ fdf.h
@@ -1,16 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/23 10:23:19 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/07/23 10:28:50 by nzharkev         ###   ########.fr       */
+/*   Created: 2024/07/31 12:54:44 by nzharkev          #+#    #+#             */
+/*   Updated: 2024/07/31 13:00:57 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main()
-{
-	
-}
+#ifndef FDF_H
+# define FDF_H
+# include "../MLX42/include/MLX42/MLX42.h"
+# include "../libft/libft.h"
+# include "../get_next_line/get_next_line.h"
+# include "../ft_printf/ft_printf.h"
+
+
+#endif
